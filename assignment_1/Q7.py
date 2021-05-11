5b62e3c0cd76fda71def87b4d36b2859758a2079
himIoT19 <thehimu1990@gmail.com>
2021-05-11 19:20:25 +0530

    Fix: Refactored Q7 and Q8.

--- assignment_1/Q7.py
+++ assignment_1/Q7.py
@@ -1,27 +1,30 @@
-#calculate number of uppercase and lowercase letters
-# Python3 program to count upper and
-# lower case characters without using
-# inbuilt functions
-def upperlower(string):
-    upper = 0
-    lower = 0
+# Program to count upper and lower case characters without using inbuilt functions
 
-    for i in range(len(string)):
+def count_upper_lower_characters(str_1):
+    """
+    Function to count upper and lower case characters without using inbuilt functions
+    :param str_1: String
+    :return: None
+    """
+    # Flags for
+    char_count_uppercase = 0
+    char_count_lowercase = 0
 
-        # For lower letters
-        if (ord(string[i]) >= 97 and
-                ord(string[i]) <= 122):
-            lower += 1
+    for i in range(len(str_1)):
 
-        # For upper letters
-        elif (ord(string[i]) >= 65 and
-              ord(string[i]) <= 90):
-            upper += 1
+        # For lowercase characters
+        if 97 <= ord(str_1[i]) <= 122:
+            char_count_lowercase += 1
 
-    print('Lower case characters = %s' % lower,
-          'Upper case characters = %s' % upper)
+        # For uppercase characters
+        elif 65 <= ord(str_1[i]) <= 90:
+            char_count_uppercase += 1
+
+    print(f'Lower case characters: {char_count_lowercase} \nUpper case characters: {char_count_uppercase}')
 
 
 # Driver Code
-string = 'HeLLo WorLD Upper'
-upperlower(string)
+string_1 = 'HeLLo WorLD Upper'
+
+# Function call
+count_upper_lower_characters(string_1)
--- assignment_1/Q8.py
+++ assignment_1/Q8.py
@@ -1,12 +1,22 @@
-#remove the duplicate elements from the list
-def Remove(duplicate):
+# Program to remove the duplicate elements from the list
+
+def remove(list_1):
+    """
+    Remove the duplicate elements from the list
+    :param list_1:
+    :return: None
+    """
     unique = []
-    for num in duplicate:
-        if num not in unique:
-            unique.append(num)
+    for item in list_1:
+        if item not in unique:
+            unique.append(item)
+    print(f"length of new list: {len(unique)}")
     return unique
 
 
 # Driver Code
-duplicate = [1,2,3,3,3,3,4,5]
-print(Remove(duplicate))
+duplicate = ["delhi", 1, 2, 'a', 3, 3, "Delhi", 3, 3, 4, 5, 'a', "Delhi"]
+print(f"length of previous list: {len(duplicate)}")
+
+# Function call
+print(remove(duplicate))

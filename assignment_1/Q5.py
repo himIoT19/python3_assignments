abcd2af3099453cdfbed39a12a5b8bf4f7509293
himIoT19 <thehimu1990@gmail.com>
2021-05-11 18:55:56 +0530

    Fix: Refactored Q5 and Q6.

--- assignment_1/Q5.py
+++ assignment_1/Q5.py
@@ -1,10 +1,22 @@
-#factorial of a number
+# factorial of a number
 
-def fact(n):
-    if(n==0|n==1):
-        return  1
+def fact(n: int) -> int:
+    """
+    Function used to get factorial
+    :param n:
+    :return:
+    """
+    if n < 0:
+        return 0
+    elif n == 0 or n == 1:
+        return 1
     else:
-        return  n*fact(n-1)
+        return n * fact(n - 1)
 
 
-print(fact(5))
+number = 5
+output = fact(number)
+if output == 0:
+    print("The entered number to find factorial is negative")
+else:
+    print(f"Factorial of {number} is: {output}")
--- assignment_1/Q6.py
+++ assignment_1/Q6.py
@@ -1,7 +1,16 @@
-#check the number is in given range
-def check_range(n):
-    if n in range(10,20):
-        print( "Number %s is in the range"%str(n))
-    else :
+# check the number is in given range
+
+def check_range(n: int):
+    """
+    Function to find whether the number is present in the provide range
+    :param n:
+    :return:
+    """
+    if n in range(10, 20):
+        print(f"Number {n} is in the range.")
+    else:
         print("The number is not in given range.")
+
+
+# Function call
 check_range(15)

34b16808192137658f61069268b3ccacca19922c
himIoT19 <thehimu1990@gmail.com>
2021-05-12 09:23:59 +0530

    Fix: Refactored Q11 and Q12.

--- assignment_1/Q11.py
+++ assignment_1/Q11.py
@@ -1,10 +1,17 @@
-#check given number is perfect or not
+# Program to check given number is perfect or not
+# Note: In number theory, a perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself.
+# For instance, 6 has divisors 1, 2 and 3 (excluding itself), and 1 + 2 + 3 = 6, so 6 is a perfect number.
+
 n = int(input("Enter any number: "))
-sum = 0
+output_sum = 0
+
+# Iterate to find divisors and sum the divisors
 for i in range(1, n):
-    if(n % i == 0):
-        sum = sum + i
-if (sum == n):
-    print("perfect")
+    if n % i == 0:
+        output_sum = output_sum + i
+
+# Condition for perfect number
+if output_sum == n:
+    print(f"{n} is a perfect number")
 else:
-    print("not")
+    print(f"{n} is not a perfect number")
--- assignment_1/Q12.py
+++ assignment_1/Q12.py
@@ -1,14 +1,24 @@
-#palindrome string
+# Program to find the string is a palindrome string
 
-def isPalindrome(s):
-	return s == s[::-1]
+def is_palindrome(str_1: str) -> bool:
+    """
+    Function to find the string is a palindrome string
+    :param str_1: String
+    :return: boolean True/False
+    """
+    if str_1 == str_1[::-1]:
+        return True
+    return False
 
 
 # Driver code
 s = "madam or nurses run"
-ans = isPalindrome(s)
+# s = 'madam'
+
+# Function call and save the result returned by the function call
+ans = is_palindrome(s)
 
 if ans:
-	print("Yes")
+    print("Yes, the string is a palindrome string")
 else:
-	print("No")
+    print("No, the string is not a palindrome string")
